3d41cc38b2da3729a003a94dcadbdbd14f454608
Harshavardhan Ushakola <harshavardhan.ushakola14@gmail.com>
2025-08-30 16:59:27 +0530

    NEW MODIFICATION

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/USERS-DATA")
+mongoose.connect(process.env.MONGO_URL)
 .then(()=>{
     console.log("Connection Sucessfull");
 })
