1b8019cde5030f90ebb65f8747e4de07cfb9e8ba
Harshavardhan Ushakola <harshavardhan.ushakola14@gmail.com>
2025-09-01 15:00:12 +0530

    CHANGES in Index.js

--- backend/index.js
+++ backend/index.js
@@ -35,7 +35,7 @@ const port = process.env.PORT || 5000 ;
 
 const io = new Server(server,{
     cors:{
-        origin:"http://localhost:5173",
+        origin:["http://localhost:5173",process.env.FRONTEND_URL],
         methods:["GET","POST"],
         credentials:true,
     }
