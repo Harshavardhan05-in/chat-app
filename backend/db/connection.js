a3a0ddb65ddda9b339f2135fef8182e77bc7ca9e
Harshavardhan Ushakola <harshavardhan.ushakola14@gmail.com>
2025-12-27 23:13:54 +0530

    new change

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URL)
 .then(()=>{
-    console.log("Connection Sucessfull");
+    console.log("Connection Sucessfull!");
 })
 .catch(()=>{
     console.log(" Not Connected")
